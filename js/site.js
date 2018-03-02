0a227f0241212f8ee51e342539758ba55cdcab22
idealatom <idealatom@gmail.com>
2018-03-02 22:34:29 +0300

    update countdown timer date

--- js/site.js
+++ js/site.js
@@ -1,34 +1,33 @@
 $(document).ready(function(){
 
-var cd = new Countdown({
-    cont: document.querySelector('#countdown-timer'),
-    endDate: {
-        year: 2018,
-        month: 3,
-        day: 3,
-        hour: 12,
-        minute: 0,
-        second: 0,
-    },
-    outputTranslation: {
-        year: 'Years',
-        week: 'Weeks',
-        day: 'Days',
-        hour: 'Hours',
-        minute: 'Minutes',
-        second: 'Seconds',
-    },
-    endCallback: null,
-    outputFormat: 'day|hour|minute|second',
-});
-cd.start();
+var endDate = new Date("2018-03-01T23:59:59-08:00")
+var now = new Date();
+if (endDate > now) {
+    var cd = new Countdown({
+        cont: document.querySelector('#countdown-timer'),
+        endDate: endDate.getTime(),
+        outputTranslation: {
+            year: 'Years',
+            week: 'Weeks',
+            day: 'Days',
+            hour: 'Hours',
+            minute: 'Minutes',
+            second: 'Seconds',
+        },
+        endCallback: null,
+        outputFormat: 'day|hour|minute|second',
+    });
+    cd.start();
 
-// close floating container
-$(document).on('click', '.floating-btn-container .modal-close-btn', function() {
-  $('.floating-btn-container').fadeOut('fast');
-})
+    // close floating container
+    $(document).on('click', '.floating-btn-container .modal-close-btn', function() {
+      $('.floating-btn-container').fadeOut('fast');
+    })
 
-$(document).on('click', '.w-dropdown', function() {
-  $(this).find('.w-dropdown-list').toggle('fast');
-})
+    $(document).on('click', '.w-dropdown', function() {
+      $(this).find('.w-dropdown-list').toggle('fast');
+    })
+} else {
+    $('.timer-title').text('Token sale in progress');
+}
 });
